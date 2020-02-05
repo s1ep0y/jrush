642adde1e24419b46aecb3be73e7d7f609e49243
s1ep0y <danmol04@gmail.com>
2020-02-05 15:12:42 +0300

    baselayer and webpack config

---
+++ block5/src/app.js
@@ -0,0 +1,4 @@
+import 'normalize.css';
+import './js/index.js'
+import './scss/main.scss'
+import './css/style.css'
---
+++ block5/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader, {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }, {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                            config: {
+                                path: './postcss.config.js'
+                            }
+                        }
+                    }
+                ]
+            }, {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader, {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }, {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                            config: {
+                                path: 'src/js/postcss.config.js'
+                            }
+                        }
+                    }, {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }
+        ]
+
+    },
+    plugins: [new MiniCssExtractPlugin({filename: "[name].css"})]
+};
