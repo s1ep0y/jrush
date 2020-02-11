35eac3105903eef6af26d0825bbc0b25fcd1f79a
s1ep0y <danmol04@gmail.com>
2020-02-11 14:09:37 +0300

    one more rebuild and animations

--- block5/src/js/index.js
+++ block5/src/js/index.js
@@ -11,10 +11,13 @@ hideMenu = () => {
 };
 
 showText = () =>{
-
+    document.getElementById('header-text-control').classList.add('showtext');
+    document.getElementById('read-more').style.display='none';
+    document.getElementById('read-less').style.display='flex';
 }
 
 hideText = () =>{
-    
+    document.getElementById('header-text-control').classList.remove('showtext');
+    document.getElementById('read-less').style.display='none';
+    document.getElementById('read-more').style.display='flex';
 }
-console.log('stop wokr')
