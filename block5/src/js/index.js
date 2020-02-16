81cc932e7094d8277ec42a795ce8126bc3e55b79
s1ep0y <danmol04@gmail.com>
2020-02-16 03:25:58 +0300

    full base, now need modals

--- block5/src/js/index.js
+++ block5/src/js/index.js
@@ -26,27 +26,37 @@ document.querySelectorAll('.show-hide-buttons').forEach(item=>{
 
 var isSwiperWork = undefined;
 
-function sliderControl(target){
-    console.log(isSwiperWork)
-    // todo - заставить эту херню нормально удаляться. Шел второй час
+function sliderControl(){
     if(window.innerWidth < 768){
         isSwiperWork = true;
-        document.querySelectorAll(`.${target}__container`).forEach(elem=>{
+        document.querySelectorAll(`.content-body__container`).forEach(elem=>{
             elem.classList.add('swiper-container')
         })
-        document.querySelectorAll(`.${target}__wrapper`).forEach(elem=>{
+        document.querySelectorAll(`.prices__container`).forEach(elem=>{
+            elem.classList.add('swiper-container')
+        })
+        document.querySelectorAll(`.content-body__wrapper`).forEach(elem=>{
             elem.classList.add('swiper-wrapper')
         })
+
         
-        document.querySelectorAll(`.${target}__list-item`).forEach(element => {
+        
+        document.querySelectorAll(`.content-body__list-item`).forEach(element => {
         element.classList.add('swiper-slide')
         });
-        
+
+
+        document.querySelectorAll(`.prices__list-item`).forEach(element => {
+            element.classList.add('swiper-slide')
+            });
+            document.querySelectorAll(`.prices__wrapper`).forEach(elem=>{
+                elem.classList.add('swiper-wrapper')
+            })
             let mySwiper = new Swiper ('.swiper-container',{
             slidesPerView: 'auto',
             spaceBetween: 16,
             autoplay: {
-                delay: 2000,
+                delay: 7000,
             },
             loop: true,
             pagination: {
@@ -58,18 +68,28 @@ function sliderControl(target){
     }
     
         if(isSwiperWork){
-            let mySwiper = document.querySelector('.swiper-container').swiper;
-            mySwiper.destroy(true, true);
-            document.querySelectorAll(`.${target}__container`).forEach(elem=>{
+            document.querySelectorAll('.swiper-container').forEach(element=>{
+                element.swiper.destroy(true, true);
+            })
+            document.querySelectorAll(`.prices__container`).forEach(elem=>{
                 elem.classList.remove('swiper-container')
             })
-            document.querySelectorAll(`.${target}__wrapper`).forEach(elem=>{
+            document.querySelectorAll(`.content-body__container`).forEach(elem=>{
+                elem.classList.remove('swiper-container')
+            })
+            document.querySelectorAll(`.content-body__wrapper`).forEach(elem=>{
                 elem.classList.remove('swiper-wrapper')
             })
             
-            document.querySelectorAll(`.${target}__list-item`).forEach(element => {
+            document.querySelectorAll(`.content-body__list-item`).forEach(element => {
             element.classList.remove('swiper-slide')
             });
+            document.querySelectorAll(`.prices__list-item`).forEach(element => {
+                element.classList.remove('swiper-slide')
+                });
+                document.querySelectorAll(`.prices__wrapper`).forEach(elem=>{
+                    elem.classList.remove('swiper-wrapper')
+                })
             isSwiperWork = false
             return
         }
